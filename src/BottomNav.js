eb802d1dfb7a67462b97f733b20caf4549f00936
simonsajji <85159792+simonsajji@users.noreply.github.com>
2022-03-02 00:06:57 +0530

    Update BottomNav.js

--- src/BottomNav.js
+++ src/BottomNav.js
@@ -16,10 +16,10 @@ class BottomNav extends React.Component {
                 <div className="icon">Instagram</div>
                 <div className="search"> <input type="text" placeholder="Search" id="s"></input></div>
                 <div className="icons">
-                    <button className="btns" onClick={()=> this.props.switchTab("Home",this.homeRef.current)} value="home" ref={this.homeRef} id="0"> <img src="./icons/instagram-home-icon-by-Vexels.png"></img> </button>
-                    <button className="btns" onClick={()=> this.props.switchTab("Reels",this.reelsRef.current)} value="Reels" ref={this.reelsRef} id="1"> <img src="./icons/instagram-reels.png"></img> </button>
-                    <button className="btns" onClick={() => this.props.switchTab("Activity",this.activityRef.current)} value="Activity" ref={this.activityRef} id="2"> <img src="./icons/heart-thin.png"></img> </button>
-                    <button className="btns" onClick={()=> this.props.switchTab("Profile",this.profileRef.current)} value="Profile" ref={this.profileRef} id="3"> <img src="./icons/account.png"></img> </button>
+                    <button className="btns" onClick={()=> this.props.switchTab("Home",this.homeRef.current)} value="home" ref={this.homeRef} id="0"> <img src="./src/instagram-home-icon-by-Vexels.png"></img> </button>
+                    <button className="btns" onClick={()=> this.props.switchTab("Reels",this.reelsRef.current)} value="Reels" ref={this.reelsRef} id="1"> <img src="./src/instagram-reels.png"></img> </button>
+                    <button className="btns" onClick={() => this.props.switchTab("Activity",this.activityRef.current)} value="Activity" ref={this.activityRef} id="2"> <img src="./src/heart-thin.png"></img> </button>
+                    <button className="btns" onClick={()=> this.props.switchTab("Profile",this.profileRef.current)} value="Profile" ref={this.profileRef} id="3"> <img src="./src/account.png"></img> </button>
                 </div>
                 
             </div>
