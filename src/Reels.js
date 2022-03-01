54150d37aca41618aaf357cede1bb848720112ae
simonsajji <85159792+simonsajji@users.noreply.github.com>
2022-03-01 23:33:35 +0530

    Update Reels.js

--- src/Reels.js
+++ src/Reels.js
@@ -4,46 +4,46 @@ class Reels extends React.Component {
         return (
             <div className="Reels">
 
-               <div ><img src="./images/image5.jpg"></img></div> 
+               <div ><img src="image5.jpg"></img></div> 
                <div className='dec'>
 
-                    <div><img src="./images/image3.jpg"></img></div> 
-                    <div><img src="./images/image10.jpg"></img></div> 
+                    <div><img src="image3.jpg"></img></div> 
+                    <div><img src="image10.jpg"></img></div> 
 
 
                 </div>
-               <div><img src="./images/image11.jpg"></img></div> 
-               <div><img src="./images/image9.jpg"></img></div> 
+               <div><img src="image11.jpg"></img></div> 
+               <div><img src="image9.jpg"></img></div> 
                <div className='inc'>
 
-                    <div><img src="./images/image3.jpg"></img></div> 
-                    <div><img src="./images/image1.jpg"></img></div> 
-                    <div ><img src="./images/image2.jpg"></img></div> 
-                    <div><img src="./images/image3.jpg"></img></div> 
+                    <div><img src="image3.jpg"></img></div> 
+                    <div><img src="image1.jpg"></img></div> 
+                    <div ><img src="image2.jpg"></img></div> 
+                    <div><img src="image3.jpg"></img></div> 
                     
                 </div> 
-               <div><img src="./images/image2.jpg"></img></div> 
+               <div><img src="image2.jpg"></img></div> 
                <div className='dec'>
 
-                    <div><img src="./images/image3.jpg"></img></div> 
-                    <div><img src="./images/image7.jpg"></img></div> 
+                    <div><img src="image3.jpg"></img></div> 
+                    <div><img src="image7.jpg"></img></div> 
                     
                     
                 </div>
              
-               <div><img src="./images/image8.jpg"></img></div> 
-               <div ><img src="./images/image13.jpg"></img></div> 
-               <div><img src="./images/image12.jpg"></img></div> 
+               <div><img src="image8.jpg"></img></div> 
+               <div ><img src="image13.jpg"></img></div> 
+               <div><img src="image12.jpg"></img></div> 
                <div className='inc'>
 
-                    <div><img src="./images/image13.jpg"></img></div> 
-                    <div><img src="./images/image12.jpg"></img></div> 
-                    <div ><img src="./images/image2.jpg"></img></div> 
-                    <div><img src="./images/image3.jpg"></img></div> 
+                    <div><img src="image13.jpg"></img></div> 
+                    <div><img src="image12.jpg"></img></div> 
+                    <div ><img src="image2.jpg"></img></div> 
+                    <div><img src="image3.jpg"></img></div> 
                     
                 </div>
-               <div ><img src="./images/image15.jpg"></img></div> 
-               <div><img src="./images/image8.jpg"></img></div>
+               <div ><img src="image15.jpg"></img></div> 
+               <div><img src="image8.jpg"></img></div>
                 
                 
             </div>
